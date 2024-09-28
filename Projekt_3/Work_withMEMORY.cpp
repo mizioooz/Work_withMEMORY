9f28a4a9d0c45f356bdf443cdf04280d912b35c7
dizarizago . <dizarizago@outlook.com>
2024-09-28 15:44:53 +0300

    update functions

--- Projekt_3/Work_withMEMORY.cpp
+++ Projekt_3/Work_withMEMORY.cpp
@@ -1,26 +1,46 @@
 ﻿#include <iostream>
+#include <math.h>
+#include <Windows.h>
 #include "dizari_math.h" // using my library
 
 using namespace std;
 
 int main()
 {
-	char SW = 0;
-	Dizari_print_fullnum_charFLAG(SW);//1-st line console 
-	// we can use 00000000 8 positions/8 bit
-	SW |= 0x04;// on 3 bit 00000100 2^2=4 => 0x04 in 16CC
-	SW |= 0x01;// on 1 bit 00000101 2^0=1 => 0x01 in 16CC
-	SW |= 0x08;// on 4 bit 00001101 2^3=8 => 0x08 in 16CC
+	//char SW = 0;
+	//Dizari_print_fullnum_charFLAG(SW);//1-st line console 
+	//// we can use 00000000 8 positions/8 bit
+	//SW |= 0x04;// on 3 bit 00000100 2^2=4 => 0x04 in 16CC
+	//SW |= 0x01;// on 1 bit 00000101 2^0=1 => 0x01 in 16CC
+	//SW |= 0x08;// on 4 bit 00001101 2^3=8 => 0x08 in 16CC
 
-	//fast(2CC to 10CC)
-	cout << int(SW) << endl;// 00001101 = 8 + 4 + 1 = 13 
-	Dizari_print_fullnum_charFLAG(SW);
+	////fast(2CC to 10CC)
+	//cout << int(SW) << endl;// 00001101 = 8 + 4 + 1 = 13 
+	//Dizari_print_fullnum_charFLAG(SW);
 
-	//an example of how the algorithm works
-	if (Dizari_charFLAG(SW, 1) == 0)
-		cout << "False" << endl;
-	else
-		cout << "True" << endl;
+	////an example of how the algorithm works
+	//if (Dizari_charFLAG(SW, 1) == 0)
+	//	cout << "False" << endl;
+	//else
+	//	cout << "True" << endl;
+	//cout << endl;
 
+	//cheking update massivs in functions Dizari 
+	//max for this func // for hex79 // for dec 121 //
+	//for bin 0111 1001
+	char SU = 0;
+	cout << pow(2, 7);
+	SU |= 0x40; // 01111001
+
+	cout << int(SU) << endl;
+	system("cls");
+	Dizari_print_fullnum_charFLAG(SU);
+
+	for (int i=6; i>=0; i--)
+		if (Dizari_charFLAG(SU, i) == 0)
+			cout << "False" << endl;
+		else
+			cout << "True" << endl;
+		cout << endl;
 	return 0;
 }
--- Projekt_3/dizari_math.h
+++ Projekt_3/dizari_math.h
@@ -5,10 +5,22 @@ using namespace std;
 
 //functions for flags
 // dec -> bin
+//max for this func 
+//for hex79 // for dec 121 //for bin 0111 1001
+// 
+// There should be nothing higher if we consider the bit part
+//40 = 0100 0000
+//20 = 0010 0000
+//10 = 0001 0000
+//08 = 0000 1000
+//04 = 0000 0100
+//02 = 0000 0010
+//01 = 0000 0001
+
 int Dizari_charFLAG(int n, int z)
 {
 	const int pos = z;
-	int binaryNum[32];
+	int binaryNum[8];
 	int i = 0;
 	while (n > 0) {
 		binaryNum[i] = n % 2;
@@ -20,7 +32,7 @@ int Dizari_charFLAG(int n, int z)
 }
 void Dizari_print_fullnum_charFLAG(int n)
 {
-	int binaryNum[32];
+	int binaryNum[8];
 	int i = 0;
 	while (n > 0) {
 		binaryNum[i] = n % 2;
